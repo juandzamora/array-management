2b626ffbd490f4ba76b42f183d8493a73b54fa9b
juandzamora <zamoraguarinjuanda@proton.me>
2023-07-04 21:16:40 -0500

    Initial functionality

--- main.cpp
+++ main.cpp
@@ -1,1 +1,12 @@
 #include "src/array.h"
+
+int main()
+{
+    array example1(); //Default 1
+    array example2( 10 ); //Size 10
+    array * example3 = new array( 15 ); //In Heap memory: size of 15
+
+    delete example3;
+
+    return 0;
+}
